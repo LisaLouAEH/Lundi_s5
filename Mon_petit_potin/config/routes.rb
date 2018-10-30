d775ccf30716f40e42fbd44ff30ec1a1697edef6
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-10-30 10:35:22 +0100

    créa page show par potin + fixe routes ok

--- Mon_petit_potin/config/routes.rb
+++ Mon_petit_potin/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: 'potin#index' #path
-  get '/team', to:"static#team"
+  get '/team', to:'static#team'
   get '/Accueil', to:"static#acceuil"
   get '/contact', to:"static#contact"
-  get '/page_potin/#{potin.id}', to:"potin#page_potin"
+  get '/show/:id', to: 'potin#show'
+  #get 'potins/:id', to: 'potins#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
